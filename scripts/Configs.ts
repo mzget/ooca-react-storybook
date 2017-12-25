27199bda0e8d50c281eab738bf059f866f084621
Nattapon Rattajak <rattajak.n@gmail.com>
2017-12-26 00:11:40 +0700

    update storybook.

--- scripts/Configs.ts
+++ scripts/Configs.ts
@@ -1,14 +1,23 @@
 /* eslint-disable */
+
 const dev_api = "https://private-anon-65a255ffbb-ooca.apiary-mock.com";
+
 const production_api = "http://api.ooca.co";
+
 const header = { 'Content-Type': 'application/json', };
 //'Authorization':Bearer{jwt token}
 //,'content-type':'application/x-www-form-urlencoded'
 //'x-master-key': `smelink1234`,'Cache-Control':['no-cache','no-store','must-revalidate',]
+
 const dev = {
     api_feedback: `${dev_api}/api/appointments`,
-};
+}
+
 const production = {
     api_feedback: `${production_api}/api/appointments`,
-};
-export const Config = function () { return dev; };
+}
+
+export const Config = function () { return dev; }
+
+
+
